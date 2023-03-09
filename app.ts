28593f60c7f19df4d40e8725c89d7df97df1e371
Matias Cagliero <matucagliero95@gmail.com>
2023-03-09 00:05:31 -0300

    nuevos metodos para explorar

--- app.ts
+++ app.ts
@@ -9,6 +9,11 @@ gestor.darDeAlta("renault", "456 asd", 1810)
 //console.log(biblioteca)
 gestor.darDeAlta("fiat", "213 fgd", 1816)
 
-gestor.consultar("fiat", biblioteca)
-gestor.actualizar("456 asd", "renoleta", 2023, biblioteca)
+//gestor.consultar("fia", biblioteca);
+gestor.eliminar("renault", "455 asd", biblioteca);
+gestor.actualizar("456 asd", biblioteca, "renoleta", 2023);
 gestor.listadoDeAutos()
+gestor.explorarPorMarca("fiat")
+gestor.explorarPorPatente("123 abc")
+
+//.explorar, en gestacion, tendria que poder explorar en biblioteca por marca, y devolver algun mensaje si la marca no existe
--- registro.ts
+++ registro.ts
@@ -1,12 +1,11 @@
 import { Auto } from "./autos";
 const fs = require("fs");
 export const biblioteca: any [] = [];
-
 export class RegistroDelAutomotor{
    
     darDeAlta(marca: string, patente:string, modelo:number){
-        let auto = new Auto(marca, modelo, patente)
-        biblioteca.push(auto);
+        
+        biblioteca.push(new Auto(marca, modelo, patente));
         const datoALlevar= JSON.stringify(biblioteca);
         const llevar = fs.writeFileSync('./registroAutos.json', datoALlevar,
              {
@@ -21,36 +20,63 @@ export class RegistroDelAutomotor{
         console.log(biblioteca)
      }
 
+     explorarPorMarca(marca: string){
 
-     consultar(marca: string, array: any[],  patente?:string){
-               let autoEncontrado = array.findIndex(Auto => Auto.marca === marca || Auto.patente === patente )
-               if(autoEncontrado >= 0){
-                   console.log(marca, patente, ' Existe en bibioteca')
-               } else{
-                   console.log(marca, patente, ' No existe en biblioteca');
+            let filtro = biblioteca.filter(elem => elem.marca === marca)
+            if (filtro.length === 0) {
+                console.log(`la marca ${marca} no existe en biblioteca`)
+            } else console.log(`vehiculos marca ${marca}`, filtro);
+     }
+
+     explorarPorPatente(patente:any){
+        
+        let filtro = biblioteca.filter(elem => elem.patente === patente)
+        if (filtro.length === 0) {
+            console.log(`la patente "${patente}" no existe en biblioteca`)
+        } else console.log(`Vehiculo con patente "${patente}"`, filtro);
+       
+ }      
+    //  consultar(marca: string, array: any[],  patente?:string){
+    //            let autoEncontrado = array.findIndex(Auto => Auto.marca === marca || Auto.patente === patente )
+    //            if(autoEncontrado >= 0){
+    //                console.log(marca, patente, ' Existe en bibioteca')
+    //            } else{
+    //                console.log(marca, patente, ' No existe en biblioteca');
             
-               }
-            }
+    //            }
+    //         }
 
      eliminar(marca: string, patente: string, array: Auto[]): any {
          let autoEncontrado: number = array.findIndex(elem => elem.marca === marca && elem.patente === patente);
-        array.splice(autoEncontrado,1)
          console.log(autoEncontrado)
         if(autoEncontrado >= 0){  
-                    console.log("vehiculo marca ", marca,"patente ", patente, " eliminado de la lista");
+                array.splice(autoEncontrado,1)
+                console.log("vehiculo marca ", marca,"patente ", patente, " eliminado de la lista");
             } 
         else {
             console.log('auto ', marca, 'no ha sido eliminado');
         }
      }
      
-     actualizar(patente: string, newMarca: string, newModelo: number, array: any){
+     actualizar(patente: string, array: any, newMarca?: string, newModelo?: any){
         let pos: number = array.findIndex((elem: { patente: string; }) => elem.patente === patente);
-        biblioteca[pos].marca = newMarca;
-        array[pos].modelo = newModelo;
-        if (biblioteca[pos].marca === newMarca && biblioteca[pos].modelo === newModelo) {
-            console.log("Se actualizaron los datos del vehiculo con patente ", patente)
-        } else {"No se pudieron actualizar los datos"}
+        if(pos<0){
+            console.log("No se encontro la patente")
+        } else {
+        array[pos].marca = newMarca;
+        array[pos].modelo = parseInt(newModelo);
+        
+        if (array[pos].marca === newMarca) {
+            console.log("Se modifico la marca de el vehiculo patente ", patente)
+        } else {
+            console.log("No se pudo modificar la marca de el vehiculo patente ", patente)
+        } 
+        if (array[pos].modelo === newModelo) {
+            console.log("Se modifico el modelo de el vehiculo patente ", patente)
+        } else {
+            console.log("No se pudo modificar el modelo de el vehiculo patente ", patente)
+        }
+    }
      }
     }
 
